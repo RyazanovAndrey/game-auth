9b447130c13751009daa35108dc95b43e014a1fc
RyazanovAndrey <ryazanov.andrej@gmail.com>
2026-02-20 15:29:27 +0200

    15.29

--- src/components/ProtectedRoute.jsx
+++ src/components/ProtectedRoute.jsx
@@ -1,11 +1,13 @@
 import { Navigate } from "react-router-dom"
+import { ROUTES } from "../route"
+import { useAuth } from "../context/AuthProvider"
 
 const ProtectedRoute = ({ children }) => {
 
   const user = JSON.parse(localStorage.getItem('user')) || null
 
   if (!user) {
-    return <Navigate to={'/'} />
+    return <Navigate to={ROUTES.HOME} />
   }
 
   return children
